b2c0277d4967564ada09bed1ce19e990e0bd85f1
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-11-12 14:34:21 -0800

    refactor random facts to separate class

--- app/src/main/java/com/freeman_smith/tthfunfacts/MainActivity.java
+++ app/src/main/java/com/freeman_smith/tthfunfacts/MainActivity.java
@@ -17,6 +17,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     private TextView mFactTextView;
     private Button mNextFactButton;
+    private FactBook mFactBook = new FactBook();
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -33,22 +34,7 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver",
-                        "You were born with 300 bones; by the time you are an adult you will have 206.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit",
-                        "Mammoths still walked the earth when the Great Pyramid was being built."};
-
-                String fact = "";
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                fact = facts[randomNumber];
+                String fact = mFactBook.getFact();
                 mFactTextView.setText(fact);
             }
         };
